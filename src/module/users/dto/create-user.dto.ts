3a9f166db0679fa54abe4e8059147377b3b3463f
hboppe <hanna.nascimento31@gmail.com>
2023-06-06 19:39:23 -0400

    feat: add validators to CreateUserDTO

--- src/module/users/dto/create-user.dto.ts
+++ src/module/users/dto/create-user.dto.ts
@@ -1,8 +1,34 @@
+import {IsEmail, IsNotEmpty, IsNumberString, IsOptional, IsPhoneNumber, IsString, IsStrongPassword, MinLength, Validate} from 'class-validator'
+import { DoesItMatchPasswordConstraint } from '../validators/matches-password.validator'
+
 export class CreateUserDTO{
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsString()
   name: string
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string
+
+  @IsNotEmpty()
+  @IsStrongPassword()
   password: string
+
+  @IsNotEmpty()
+  @Validate(DoesItMatchPasswordConstraint)
+  confPassword: string
+
+  @IsOptional()
+  @IsString()
   bio: string
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @IsPhoneNumber('US')
   contact: string
+
+  @IsNotEmpty()
+  @IsString()
   module: string
 }
