56a492a215f4a5d70c339c398800750f6f244f24
hboppe <hanna.nascimento31@gmail.com>
2023-06-27 13:01:03 -0400

    fix: add type to findAll

--- src/module/users/repositories/users.repository.ts
+++ src/module/users/repositories/users.repository.ts
@@ -4,7 +4,7 @@ import { User } from "../entities/user.entity";
 
 export abstract class UserRepository{
   abstract create(userData: CreateUserDTO): Promise<User> | User
-  abstract findAll(): Promise<User[]> | User[]
+  abstract findAll(): Promise<User[]> | User[] | []
   abstract findOne(userId: string): Promise<User> | User
   abstract update(userId: string, updatedData: UpdateUserDTO): Promise<User> | User
   abstract delete(userId: string): Promise<void> | void
