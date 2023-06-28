37941d2e2321f968f35a0f8e9759d3cde68a0f32
hboppe <hanna.nascimento31@gmail.com>
2023-06-28 09:49:02 -0400

    feat: add a ValidationPipe with group transform

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true})),
+    new ValidationPipe({
+        transform: true,
+        groups: ['transform']
+    })
   await app.listen(3000);
 }
 bootstrap();
