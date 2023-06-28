be3b8c2cb7ed04c04c36b13c57f5ac848df606f5
hboppe <hanna.nascimento31@gmail.com>
2023-06-28 09:43:48 -0400

    feat: add @Exclude to password

--- src/module/users/entities/user.entity.ts
+++ src/module/users/entities/user.entity.ts
@@ -1,10 +1,14 @@
+import { Exclude } from "class-transformer"
 import { randomUUID } from "crypto"
 
 export class User {
   readonly id: string
   name: string
   email: string
+
+  @Exclude()
   password: string
+
   bio: string
   contact: string
   module: string
