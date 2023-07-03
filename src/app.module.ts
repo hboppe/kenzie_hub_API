f3110cb676272cffea5fa6e38cfa38d8fe38b316
hboppe <hanna.nascimento31@gmail.com>
2023-07-03 09:24:27 -0400

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './module/users/users.module';
+import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
