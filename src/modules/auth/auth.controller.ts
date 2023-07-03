4889385ad7e1ba41bf9aead585b04b06fbcf014b
hboppe <hanna.nascimento31@gmail.com>
2023-07-03 14:17:27 -0400

    feat: add post route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserLogin } from './dto/user-login.dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
-@Controller('auth')
+@Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @UseGuards(LocalAuthGuard)
+  async login(@Body() userLoginInfo: UserLogin){
+    return this.authService.login(userLoginInfo.email)
+  }
 }
