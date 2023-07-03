cb743c1e0b8869e0c518387bfbcbe3518c5bf91e
hboppe <hanna.nascimento31@gmail.com>
2023-07-03 14:17:59 -0400

    feat: add JwtAuthGuard to routes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Get, Param, Put, Body, Delete, HttpCode, Patch, UseInterceptors, ClassSerializerInterceptor, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, Delete, HttpCode, Patch, UseInterceptors, ClassSerializerInterceptor, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -14,22 +15,26 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll(){
     return this.usersService.findAll()
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id', ParseUUIDPipe) id: string){
     return this.usersService.findOne(id)
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updatedData: UpdateUserDTO){
     return this.usersService.update(id, updatedData)
   }
 
   @HttpCode(204)
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   delete(@Param('id') id: string) {
     return this.usersService.delete(id)
   }
