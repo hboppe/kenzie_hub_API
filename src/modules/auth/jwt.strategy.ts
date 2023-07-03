9fe7a635b8ed6366189c637d69dca9606cc98fd6
hboppe <hanna.nascimento31@gmail.com>
2023-07-03 13:02:42 -0400

    feat: add jwt Strategy

---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import 'dotenv';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRET_KEY,
+    });
+  }
+
+  async validate(email: string) {
+    return { email: email };
+  }
+}
