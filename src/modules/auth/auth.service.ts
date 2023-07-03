b3ae28f29a3b25eca1b93f54fb348a0f7c0ef0e1
hboppe <hanna.nascimento31@gmail.com>
2023-07-03 13:00:36 -0400

    feat: add AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(
+    email: string,
+    password: string,
+  ) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (user) {
+      const isPasswordMatching = await compare(password, user.password);
+
+      if (isPasswordMatching) {
+        return { email: user.email };
+      }
+    }
+
+    return null;
+  }
+
+  async login(email: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    return {
+        token: await this.jwtService.sign({email: email}, { subject: user.id })
+    }
+  } 
+}
