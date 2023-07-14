21685ffc708490d304cf6ca84658a692806a1183
hboppe <hanna.nascimento31@gmail.com>
2023-07-14 10:38:47 -0400

    test: add retrieveUser mock

---
+++ test/mocks/retrieveUser.mock.ts
@@ -0,0 +1,29 @@
+export default {
+  validUser: {
+    id: '6a76df3e-2647-4d63-845f-77651206efc9',
+    name: 'User1',
+    email: 'email1@email.com',
+    bio: 'Bio1',
+    contact: '5406789078',
+    password: 'Senh@123',
+    module: 'module 1'
+  }, 
+  allUsers : [
+    {
+      id: '6a76df3e-2647-4d63-845f-77651206efc9',
+      name: 'User1',
+      email: 'email1@email.com',
+      bio: 'Bio1',
+      contact: '5406789078',
+      module: 'module 1'
+    },
+    {
+      id: '6a76df3e-2647-4d63-845f-77651206efc8',
+      name: 'User2',
+      email: 'email2@email.com',
+      bio: 'Bio1',
+      contact: '5406789079',
+      module: 'module 2'
+    }
+  ]
+}
